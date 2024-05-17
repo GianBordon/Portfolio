bcd9904a45d4a2aeef5ae6ef90fc9c6422a3f8db
Gianfranco Bordon <shiammdp21@gmail.com>
2024-05-17 12:50:59 -0300

    Portfolio 2.0

--- script.js
+++ script.js
@@ -22,25 +22,20 @@ function efectoHabilidades(){
         let habilidades = document.getElementsByClassName("progreso");
         habilidades[0].classList.add("javascript");
         habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("sass");
-        habilidades[3].classList.add("sql");
-        habilidades[4].classList.add("wordpress");
-        habilidades[5].classList.add("photoshop");
-        habilidades[6].classList.add("illustrator");
-        habilidades[7].classList.add("reactjs");
-        habilidades[8].classList.add("firebase");
-        habilidades[9].classList.add("git");
-        habilidades[10].classList.add("comunicacion");
-        habilidades[11].classList.add("trabajo");
-        habilidades[12].classList.add("creatividad");
-        habilidades[13].classList.add("dedicacion");
-        habilidades[14].classList.add("proyecto");
-        habilidades[15].classList.add("resolutivo");
-        habilidades[16].classList.add("flexible");
-        habilidades[17].classList.add("velocidad");
+        habilidades[2].classList.add("sql");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("reactjs");
+        habilidades[5].classList.add("git");
+        habilidades[6].classList.add("comunicacion");
+        habilidades[7].classList.add("dedicacion");
+        habilidades[8].classList.add("proyecto");
+        habilidades[9].classList.add("resolutivo");
+        habilidades[10].classList.add("flexible");
+        habilidades[11].classList.add("velocidad");
     }
 }
 
 window.onscroll = function(){
     efectoHabilidades();
 }
+
